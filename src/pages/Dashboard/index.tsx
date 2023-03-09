fb68ee8bc1195c62606d130959ada7a398928c58
riordan.santos <riordan3607@gmail.com>
2023-03-09 08:59:15 -0300

    using useRef hook

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -24,6 +24,7 @@ export function Dashboard() {
   });
   const [newRepo, setNewRepo] = React.useState("");
   const [inputError, setInputError] = React.useState("");
+  const formEl = React.useRef<HTMLFormElement | null>(null);
 
   React.useEffect(() => {
     localStorage.setItem("@gitCollection:repositories", JSON.stringify(repos));
@@ -43,8 +44,11 @@ export function Dashboard() {
       const response = await api.get<GithubRepository>(`/repos/${newRepo}`);
       const repository = response.data;
       setRepos([...repos, repository]);
+      formEl.current?.reset();
       setNewRepo("");
-    } catch (e) {
+      setInputError("");
+
+    } catch {
       setInputError("Informe um username/repositorio válido");
     }
   }
@@ -52,7 +56,7 @@ export function Dashboard() {
     <>
       <img src={logo} alt="GitCollection" />
       <Title>Catálogo de repositórios do Github</Title>
-      <Form onSubmit={handleAddRepo} hasError={Boolean(inputError)}>
+      <Form ref={formEl} onSubmit={handleAddRepo} hasError={Boolean(inputError)}>
         <input
           placeholder="username/repository_name"
           onChange={handleInputChange}
