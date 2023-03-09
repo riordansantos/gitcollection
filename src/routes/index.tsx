7a7b66e394df892e1b8314eae11349658ba8e557
riordan.santos <riordan3607@gmail.com>
2023-03-09 09:26:24 -0300

    magic comments with webpack

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-const Dashboard = React.lazy(() => import("../pages/Dashboard"));
-const Repo = React.lazy(() => import("../pages/Repo"));
+const Dashboard = React.lazy(() => import(
+  /* webpackPrefetch: true */
+  /* webpackChunckName: "dashboard" */
+  "../pages/Dashboard"));
+const Repo = React.lazy(() => import(
+  /* webpackPrefetch: true */
+  /* webpackChunckName: "repo" */
+  "../pages/Repo"));
 
 export function Routes() {
   return (
